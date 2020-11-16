30f2887ac64203890f5f55f883b3d1f0aa1cefe2
Chris Chuang <chrischuang0421@gmail.com>
2020-11-16 15:16:27 +0800

    Exercise 1 - Add Counter test with ReactDOM

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -5,25 +5,31 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import Counter from '../../components/counter'
 
-test('counter increments and decrements when the buttons are clicked', () => {
-  // 🐨 create a div to render your component to (💰 document.createElement)
-  //
-  // 🐨 append the div to document.body (💰 document.body.append)
-  //
-  // 🐨 use ReactDOM.render to render the <Counter /> to the div
-  // 🐨 get a reference to the increment and decrement buttons:
-  //   💰 div.querySelectorAll('button')
-  // 🐨 get a reference to the message div:
-  //   💰 div.firstChild.querySelector('div')
-  //
-  // 🐨 expect the message.textContent toBe 'Current count: 0'
-  // 🐨 click the increment button (💰 increment.click())
-  // 🐨 assert the message.textContent
-  // 🐨 click the decrement button (💰 decrement.click())
-  // 🐨 assert the message.textContent
-  //
-  // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
+beforeEach(() => {
+  document.body.innerHTML = '';
+});
+
+test('counter increments and decrements when the buttons are clicked', () => {  
+  const div = document.createElement('div');
+  document.body.append(div);  
+  ReactDOM.render(<Counter />, div);
+
+  const [decrement, increment] = div.querySelectorAll('button');
+  const message = div.firstChild.querySelector('div');  
+
+  expect(message.textContent).toBe(`Current count: 0`);  
+  increment.dispatchEvent(new MouseEvent('click', {    
+    bubbles: true, // A Boolean indicating whether the event bubbles. 
+    cancelable: true, // A Boolean indicating whether the event can be cancelled.
+    button: 0  // Main button pressed, usually the left button or the un-initialized state
+  }))
+  expect(message.textContent).toBe(`Current count: 1`);
+  decrement.dispatchEvent(new MouseEvent('click', {    
+    bubbles: true, // A Boolean indicating whether the event bubbles. 
+    cancelable: true, // A Boolean indicating whether the event can be cancelled.
+    button: 0  // Main button pressed, usually the left button or the un-initialized state
+  }))
+  expect(message.textContent).toBe(`Current count: 0`);  
 })
 
 /* eslint no-unused-vars:0 */
