44305bc3813db846ab0f07a6b5daf6e278715b61
Chris Chuang <chrischuang0421@gmail.com>
2020-11-16 16:20:21 +0800

    Prettier exercise 1 & 2 code

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -1,35 +1,39 @@
 // simple test with ReactDOM
 // http://localhost:3000/counter
 
-import * as React from 'react'
-import ReactDOM from 'react-dom'
-import Counter from '../../components/counter'
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from '../../components/counter';
 
 beforeEach(() => {
   document.body.innerHTML = '';
 });
 
-test('counter increments and decrements when the buttons are clicked', () => {  
+test('counter increments and decrements when the buttons are clicked', () => {
   const div = document.createElement('div');
-  document.body.append(div);  
+  document.body.append(div);
   ReactDOM.render(<Counter />, div);
 
   const [decrement, increment] = div.querySelectorAll('button');
-  const message = div.firstChild.querySelector('div');  
+  const message = div.firstChild.querySelector('div');
 
-  expect(message.textContent).toBe(`Current count: 0`);  
-  increment.dispatchEvent(new MouseEvent('click', {    
-    bubbles: true, // A Boolean indicating whether the event bubbles. 
-    cancelable: true, // A Boolean indicating whether the event can be cancelled.
-    button: 0  // Main button pressed, usually the left button or the un-initialized state
-  }))
+  expect(message.textContent).toBe(`Current count: 0`);
+  increment.dispatchEvent(
+    new MouseEvent('click', {
+      bubbles: true, // A Boolean indicating whether the event bubbles.
+      cancelable: true, // A Boolean indicating whether the event can be cancelled.
+      button: 0, // Main button pressed, usually the left button or the un-initialized state
+    }),
+  );
   expect(message.textContent).toBe(`Current count: 1`);
-  decrement.dispatchEvent(new MouseEvent('click', {    
-    bubbles: true, // A Boolean indicating whether the event bubbles. 
-    cancelable: true, // A Boolean indicating whether the event can be cancelled.
-    button: 0  // Main button pressed, usually the left button or the un-initialized state
-  }))
-  expect(message.textContent).toBe(`Current count: 0`);  
-})
+  decrement.dispatchEvent(
+    new MouseEvent('click', {
+      bubbles: true, // A Boolean indicating whether the event bubbles.
+      cancelable: true, // A Boolean indicating whether the event can be cancelled.
+      button: 0, // Main button pressed, usually the left button or the un-initialized state
+    }),
+  );
+  expect(message.textContent).toBe(`Current count: 0`);
+});
 
 /* eslint no-unused-vars:0 */
--- src/__tests__/exercise/02.js
+++ src/__tests__/exercise/02.js
@@ -1,21 +1,21 @@
 // simple test with React Testing Library
 // http://localhost:3000/counter
 
-import * as React from 'react'
-import { render, fireEvent } from '@testing-library/react';
-import Counter from '../../components/counter'
+import * as React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+import Counter from '../../components/counter';
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  const { container } = render(<Counter />);  
+  const {container} = render(<Counter />);
 
-  const [decrement, increment] = container.querySelectorAll('button')
-  const message = container.firstChild.querySelector('div')
-  
-  expect(message).toHaveTextContent(/^Current count: 0$/)
+  const [decrement, increment] = container.querySelectorAll('button');
+  const message = container.firstChild.querySelector('div');
+
+  expect(message).toHaveTextContent(/^Current count: 0$/);
 
   fireEvent.click(increment);
-  expect(message).toHaveTextContent(/^Current count: 1$/)  
+  expect(message).toHaveTextContent(/^Current count: 1$/);
 
   fireEvent.click(decrement);
-  expect(message).toHaveTextContent(/^Current count: 0$/)  
-})
+  expect(message).toHaveTextContent(/^Current count: 0$/);
+});
