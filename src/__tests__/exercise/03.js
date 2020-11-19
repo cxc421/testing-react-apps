08dab082ac40db68b6b217317b449bc53291151c
Chris Chuang <chrischuang0421@gmail.com>
2020-11-19 13:55:51 +0800

    Exercise 3 - Use RTL built int queries and user-event to avoid test implementation details

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -1,21 +1,23 @@
 // Avoid implementation details
 // http://localhost:3000/counter
 
-import * as React from 'react'
-// 🐨 add `screen` to the import here:
-import {render, fireEvent} from '@testing-library/react'
-import Counter from '../../components/counter'
+import * as React from 'react';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Counter from '../../components/counter';
 
+// https://testing-playground.com/
 test('counter increments and decrements when the buttons are clicked', () => {
-  const {container} = render(<Counter />)
-  // 🐨 replace these with screen queries
-  // 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
-  const [decrement, increment] = container.querySelectorAll('button')
-  const message = container.firstChild.querySelector('div')
+  render(<Counter />);
 
-  expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
-  expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
-  expect(message).toHaveTextContent('Current count: 0')
-})
+  // 🐨 replace these with screen queries
+  const decrement = screen.getByRole('button', {name: /^decrement$/i});
+  const increment = screen.getByRole('button', {name: /^increment$/i});
+  const message = screen.getByText(/^current count/i);
+
+  expect(message).toHaveTextContent('Current count: 0');
+  userEvent.click(increment);
+  expect(message).toHaveTextContent('Current count: 1');
+  userEvent.click(decrement);
+  expect(message).toHaveTextContent('Current count: 0');
+});
