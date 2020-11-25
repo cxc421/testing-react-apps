7109ccba051eafbf81051720da1de2df28da45fe
Chris Chuang <chrischuang0421@gmail.com>
2020-11-25 14:17:21 +0800

    Exercise 4 - Using jest mock function

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -6,9 +6,8 @@ import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
-test('submitting the form calls onSubmit with username and password', () => {
-  let submittedData;
-  const handleSubmit = data => (submittedData = data);
+test('submitting the form calls onSubmit with username and password', () => {  
+  const handleSubmit = jest.fn();
   
   render(<Login onSubmit={handleSubmit} />);
   // screen.debug()
@@ -24,10 +23,8 @@ test('submitting the form calls onSubmit with username and password', () => {
   const button = screen.getByRole('button', { name: /^Submit$/i });
   userEvent.click(button);
   
-  expect(submittedData).toEqual({
-    username,
-    password
-  })
+  expect(handleSubmit).toHaveBeenCalledWith({username, password});
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
 })
 
 /*
