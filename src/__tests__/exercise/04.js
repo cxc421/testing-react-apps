a1cd2d01428b5870fbe4fe7f34cd8467fd8d28a7
Chris Chuang <chrischuang0421@gmail.com>
2020-11-25 15:34:59 +0800

    Exercise 4 - Rplace hand-made test data generator with @jackfranklin/test-data-bot

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -4,14 +4,15 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import faker from 'faker';
+import { build, fake } from '@jackfranklin/test-data-bot'
 import Login from '../../components/login'
 
-const buildLoginForm = (overrides) => ({
-  username: faker.internet.userName(),
-  password: faker.internet.password(),
-  ...overrides
-});
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password())
+  }
+})
 
 test('submitting the form calls onSubmit with username and password', () => {  
   const handleSubmit = jest.fn();
@@ -19,7 +20,7 @@ test('submitting the form calls onSubmit with username and password', () => {
   render(<Login onSubmit={handleSubmit} />);
   // screen.debug()
   
-  const { username, password } = buildLoginForm();  
+  const { username, password } = buildLoginForm();      
 
   const usernameInput = screen.getByLabelText(/^username/i);
   const passwordInput = screen.getByLabelText(/^password/i);
