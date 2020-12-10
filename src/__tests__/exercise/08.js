fedd6bc3af805f72e570c66da611b50be5320549
Chris Chuang <chrischuang0421@gmail.com>
2020-12-10 14:36:13 +0800

    Exercise 8 - refactor with @testing-library/react-hooks

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -1,48 +1,52 @@
 // testing custom hooks
 // http://localhost:3000/counter-hook
-
-import * as React from 'react';
-import {render, act} from '@testing-library/react';
+import {renderHook, act} from '@testing-library/react-hooks';
 import useCounter from '../../components/use-counter';
 
-function setup(hookOptions) {
-  const results = {};
-  function UseCounterExample() {
-    Object.assign(results, useCounter(hookOptions));
-    return null;
-  }
-  render(<UseCounterExample />);
-  return results;
-}
-
 test('exposes the count and increment/decrement functions', () => {
-  const results = setup();
+  const {result} = renderHook(useCounter);
 
-  expect(results.count).toBe(0);
-  act(() => results.increment());
-  expect(results.count).toBe(1);
-  act(() => results.decrement());
-  expect(results.count).toBe(0);
+  expect(result.current.count).toBe(0);
+  act(() => result.current.increment());
+  expect(result.current.count).toBe(1);
+  act(() => result.current.decrement());
+  expect(result.current.count).toBe(0);
 });
 
 test('allows customization of the initial count', () => {
-  const results = setup({initialCount: 5});
+  const {result} = renderHook(useCounter, {
+    initialProps: {initialCount: 5},
+  });
 
-  expect(results.count).toBe(5);
-  act(() => results.increment());
-  expect(results.count).toBe(6);
-  act(() => results.decrement());
-  expect(results.count).toBe(5);
+  expect(result.current.count).toBe(5);
+  act(() => result.current.increment());
+  expect(result.current.count).toBe(6);
+  act(() => result.current.decrement());
+  expect(result.current.count).toBe(5);
 });
 
 test('allows customization of the step', () => {
-  const results = setup({step: 2});
+  const {result} = renderHook(useCounter, {
+    initialProps: {step: 2},
+  });
 
-  expect(results.count).toBe(0);
-  act(() => results.increment());
-  expect(results.count).toBe(2);
-  act(() => results.decrement());
-  expect(results.count).toBe(0);
+  expect(result.current.count).toBe(0);
+  act(() => result.current.increment());
+  expect(result.current.count).toBe(2);
+  act(() => result.current.decrement());
+  expect(result.current.count).toBe(0);
+});
+
+test(`the step can be changed`, () => {
+  const {result, rerender} = renderHook(useCounter, {initialProps: {step: 3}});
+
+  expect(result.current.count).toBe(0);
+  act(() => result.current.increment());
+  expect(result.current.count).toBe(3);
+
+  rerender({step: 2});
+  act(() => result.current.decrement());
+  expect(result.current.count).toBe(1);
 });
 
 /* eslint no-unused-vars:0 */
