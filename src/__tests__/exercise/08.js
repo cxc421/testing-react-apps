cfc4b58bbfbabff2a4b13bb8207ce6aa3c651cbc
Chris Chuang <chrischuang0421@gmail.com>
2020-12-10 11:19:05 +0800

    Exercise 8 - setup more general fake component function

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -5,18 +5,42 @@ import * as React from 'react';
 import {render, act} from '@testing-library/react';
 import useCounter from '../../components/use-counter';
 
-test('exposes the count and increment/decrement functions', () => {
-  let results;
+function setup(hookOptions) {
+  const results = {};
   function UseCounterExample() {
-    results = useCounter();
+    Object.assign(results, useCounter(hookOptions));
     return null;
   }
-
   render(<UseCounterExample />);
+  return results;
+}
+
+test('exposes the count and increment/decrement functions', () => {
+  const results = setup();
 
   expect(results.count).toBe(0);
   act(() => results.increment());
   expect(results.count).toBe(1);
+  act(() => results.decrement());
+  expect(results.count).toBe(0);
+});
+
+test('allows customization of the initial count', () => {
+  const results = setup({initialCount: 5});
+
+  expect(results.count).toBe(5);
+  act(() => results.increment());
+  expect(results.count).toBe(6);
+  act(() => results.decrement());
+  expect(results.count).toBe(5);
+});
+
+test('allows customization of the step', () => {
+  const results = setup({step: 2});
+
+  expect(results.count).toBe(0);
+  act(() => results.increment());
+  expect(results.count).toBe(2);
   act(() => results.decrement());
   expect(results.count).toBe(0);
 });
