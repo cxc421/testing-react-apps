a0ec43bfacb138aa0e5c11a54565e72910a29df8
Chris Chuang <chrischuang0421@gmail.com>
2020-12-10 11:03:17 +0800

    Exercise 8 - create fake component to interact with hook directly

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,33 +2,23 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react';
-import {render, screen} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import {render, act} from '@testing-library/react';
 import useCounter from '../../components/use-counter';
 
-function UseCounterExample({initialCount = 0}) {
-  const {count, increment, decrement} = useCounter({initialCount});
-  return (
-    <div>
-      <span>Current Count: {count}</span>
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
-    </div>
-  );
-}
-
 test('exposes the count and increment/decrement functions', () => {
+  let results;
+  function UseCounterExample() {
+    results = useCounter();
+    return null;
+  }
+
   render(<UseCounterExample />);
 
-  const display = screen.getByText(/^Current Count/);
-  const incBtn = screen.getByRole('button', {name: 'increment'});
-  const decBtn = screen.getByRole('button', {name: 'decrement'});
-
-  expect(display).toHaveTextContent(`Current Count: 0`);
-  userEvent.click(incBtn);
-  expect(display).toHaveTextContent(`Current Count: 1`);
-  userEvent.click(decBtn);
-  expect(display).toHaveTextContent(`Current Count: 0`);
+  expect(results.count).toBe(0);
+  act(() => results.increment());
+  expect(results.count).toBe(1);
+  act(() => results.decrement());
+  expect(results.count).toBe(0);
 });
 
 /* eslint no-unused-vars:0 */
