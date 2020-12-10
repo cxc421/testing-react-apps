872e93af4cb45e6d0ef9037fd43ec4d28ce56797
Chris Chuang <chrischuang0421@gmail.com>
2020-12-10 10:47:53 +0800

    Exercise 8 - create sample component to indirectly test custom hook

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -1,22 +1,34 @@
 // testing custom hooks
 // http://localhost:3000/counter-hook
 
-import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import useCounter from '../../components/use-counter'
+import * as React from 'react';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import useCounter from '../../components/use-counter';
 
-// 🐨 create a simple function component that uses the useCounter hook
-// and then exposes some UI that our test can interact with to test the
-// capabilities of this hook
-// 💰 here's how to use the hook:
-// const {count, increment, decrement} = useCounter()
+function UseCounterExample({initialCount = 0}) {
+  const {count, increment, decrement} = useCounter({initialCount});
+  return (
+    <div>
+      <span>Current Count: {count}</span>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+    </div>
+  );
+}
 
 test('exposes the count and increment/decrement functions', () => {
-  // 🐨 render the component
-  // 🐨 get the elements you need using screen
-  // 🐨 assert on the initial state of the hook
-  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
-})
+  render(<UseCounterExample />);
+
+  const display = screen.getByText(/^Current Count/);
+  const incBtn = screen.getByRole('button', {name: 'increment'});
+  const decBtn = screen.getByRole('button', {name: 'decrement'});
+
+  expect(display).toHaveTextContent(`Current Count: 0`);
+  userEvent.click(incBtn);
+  expect(display).toHaveTextContent(`Current Count: 1`);
+  userEvent.click(decBtn);
+  expect(display).toHaveTextContent(`Current Count: 0`);
+});
 
 /* eslint no-unused-vars:0 */
